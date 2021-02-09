97ba49459762d976bbc8ed99472a19af918e7c2b
Gunnar <gunnarkam621@gmail.com>
2021-02-09 14:29:05 -0500

    updated methoids

--- src/com/company/schmidt/Main.java
+++ src/com/company/schmidt/Main.java
@@ -3,22 +3,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        fahrenheit();
+       int fTemp =  getUserInput();
+        fahrenheit(fTemp);
     }
-    static void fahrenheit(){
+    static int getUserInput(){
         Scanner sc = new Scanner(System.in);
-        double temp;
-
-        for (;;){
-            System.out.println("Enter a temp in fahrenheit: ");
-            temp = sc.nextInt();
-            double celsius = 5*(temp-32)/9;
-            System.out.println(celsius);
-            if (temp<-460) {
-                System.out.println("You entered a number lower than absolute 0");
-                break;
-            }
-        }
-
+        System.out.println("enter a temp in fahrenheit");
+        int temp = sc.nextInt();
+        return temp;
     }
+    static void fahrenheit(double a){
+
+        while (a>-460){
+            double celsius = 5*(a-32)/9;
+            displayOutput(celsius);
+            a = getUserInput();
+        }
+        System.out.println("what you entered was below -460");
+    }
+    static void displayOutput(double a){
+        System.out.println(a);
+    }
+
+
+
+
 }
